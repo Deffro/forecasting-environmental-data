8df9f6386a4420b00b3efefa16e7905ff31ce695
Deffro <dimitris.effrosynidis@gmail.com>
2022-02-28 11:28:33 +0200

    now ML models set their window_length parameter from tuning

--- src/train-predict-evaluate.py
+++ src/train-predict-evaluate.py
@@ -83,93 +83,140 @@ fh=[i+1 for i in range (fh)]
 data, seasonal_period, freq_sktime = read_file(dataset_name, data_path='../data/')
 preprocess = False
 
-# for multi step consider DirectTabularRegressionForecaster, RecursiveTabularRegressionForecaster, DirRecTabularRegressionForecaster
-forecasters = [
-    NaiveForecaster(sp=1, strategy='last', window_length=None),
-    NaiveForecaster(sp=12, strategy='last', window_length=None),
-    PolynomialTrendForecaster(degree=1, regressor=None, with_intercept=True),
-    AutoARIMA(n_jobs=-1),
-    ExponentialSmoothing(damped_trend=False, initial_level=None, initial_seasonal=None, 
-                         initial_trend=None, initialization_method='estimated', seasonal=None, 
-                         sp=seasonal_period, trend='add', use_boxcox=None),
-    AutoETS(n_jobs=-1),
-    ThetaForecaster(deseasonalize=True, initial_level=None, sp=seasonal_period),
-    DirectTabularRegressionForecaster(estimator=LinearRegression(copy_X=True, fit_intercept=True, 
-                                                                 n_jobs=-1, normalize=False, positive=False)),
-    DirectTabularRegressionForecaster(estimator=Lasso(alpha=1.0, copy_X=True, fit_intercept=True, max_iter=1000, 
-                                                      normalize=False, positive=False, precompute=False, 
-                                                      random_state=42, selection='cyclic', tol=0.0001, 
-                                                      warm_start=False)),
-    DirectTabularRegressionForecaster(estimator=Ridge(alpha=1.0, copy_X=True, fit_intercept=True, max_iter=None, 
-                                                      normalize=False, random_state=42, solver='auto', tol=0.001)),
-    DirectTabularRegressionForecaster(estimator=ElasticNet(alpha=1.0, copy_X=True, fit_intercept=True, l1_ratio=0.5, 
-                                                           max_iter=1000, normalize=False, positive=False, 
-                                                           precompute=False, random_state=42, selection='cyclic', 
-                                                           tol=0.0001, warm_start=False)),
-    DirectTabularRegressionForecaster(estimator=Lars(copy_X=True, eps=2.220446049250313e-16, fit_intercept=True, 
-                                                     fit_path=True, jitter=None, n_nonzero_coefs=500, normalize=True, 
-                                                     precompute='auto', random_state=42, verbose=False)),
-    DirectTabularRegressionForecaster(estimator=LassoLars(alpha=1.0, copy_X=True, eps=2.220446049250313e-16, 
-                                                          fit_intercept=True, fit_path=True, jitter=None, 
-                                                          max_iter=500, normalize=True, positive=False, 
-                                                          precompute='auto', random_state=42, verbose=False)),
-    DirectTabularRegressionForecaster(estimator=BayesianRidge(alpha_1=1e-06, alpha_2=1e-06, alpha_init=None, 
-                                                              compute_score=False, copy_X=True, fit_intercept=True, 
-                                                              lambda_1=1e-06, lambda_2=1e-06, lambda_init=None, 
-                                                              n_iter=300, normalize=False, tol=0.001, verbose=False)),
-    DirectTabularRegressionForecaster(estimator=HuberRegressor(alpha=0.0001, epsilon=1.35, fit_intercept=True, 
-                                                               max_iter=100, tol=1e-05, warm_start=False)),
-    DirectTabularRegressionForecaster(estimator=PassiveAggressiveRegressor(C=1.0, average=False, early_stopping=False, 
-                                                                           epsilon=0.1, fit_intercept=True, 
-                                                                           loss='epsilon_insensitive', max_iter=1000, 
-                                                                           n_iter_no_change=5, random_state=42, shuffle=True, 
-                                                                           tol=0.001, validation_fraction=0.1, verbose=0, 
-                                                                           warm_start=False)),
-    DirectTabularRegressionForecaster(estimator=OrthogonalMatchingPursuit(fit_intercept=True, n_nonzero_coefs=None, 
-                                                                          normalize=True, precompute='auto', tol=None)),
-    DirectTabularRegressionForecaster(estimator=KNeighborsRegressor(algorithm='auto', leaf_size=30, metric='minkowski', 
-                                                                    metric_params=None, n_jobs=-1, n_neighbors=5, p=2, 
-                                                                    weights='uniform')),
-    DirectTabularRegressionForecaster(estimator=DecisionTreeRegressor(ccp_alpha=0.0,  criterion='mse', max_depth=None, 
-                                                                      max_features=None, max_leaf_nodes=None, 
-                                                                      min_impurity_decrease=0.0, min_impurity_split=None, 
-                                                                      min_samples_leaf=1, min_samples_split=2, 
-                                                                      min_weight_fraction_leaf=0.0, random_state=42, 
-                                                                      splitter='best')),
-    DirectTabularRegressionForecaster(estimator=RandomForestRegressor(bootstrap=True, ccp_alpha=0.0, criterion='mse', 
-                                                                      max_depth=None, max_features='auto', max_leaf_nodes=None, 
-                                                                      max_samples=None, min_impurity_decrease=0.0, 
-                                                                      min_impurity_split=None, min_samples_leaf=1, 
-                                                                      min_samples_split=2, min_weight_fraction_leaf=0.0, 
-                                                                      n_estimators=100, n_jobs=-1, oob_score=False, 
-                                                                      random_state=42, verbose=0, warm_start=False)),
-    DirectTabularRegressionForecaster(estimator=ExtraTreesRegressor(bootstrap=False, ccp_alpha=0.0, criterion='mse', 
-                                                                    max_depth=None, max_features='auto', max_leaf_nodes=None, 
-                                                                    max_samples=None, min_impurity_decrease=0.0, 
-                                                                    min_impurity_split=None, min_samples_leaf=1, 
-                                                                    min_samples_split=2, min_weight_fraction_leaf=0.0, 
-                                                                    n_estimators=100, n_jobs=-1, oob_score=False, 
-                                                                    random_state=42, verbose=0, warm_start=False)),
-    DirectTabularRegressionForecaster(estimator=GradientBoostingRegressor(alpha=0.9, ccp_alpha=0.0, criterion='friedman_mse', 
-                                                                          init=None, learning_rate=0.1, loss='ls', max_depth=3, 
-                                                                          max_features=None, max_leaf_nodes=None, 
-                                                                          min_impurity_decrease=0.0, min_impurity_split=None, 
-                                                                          min_samples_leaf=1, min_samples_split=2, 
-                                                                          min_weight_fraction_leaf=0.0, n_estimators=100, 
-                                                                          n_iter_no_change=None, random_state=42, subsample=1.0, 
-                                                                          tol=0.0001, validation_fraction=0.1, verbose=0, 
-                                                                          warm_start=False)),
-    DirectTabularRegressionForecaster(estimator=AdaBoostRegressor(base_estimator=None, learning_rate=1.0, loss='linear', 
-                                                                  n_estimators=50, random_state=42)),
-    DirectTabularRegressionForecaster(estimator=lgbm.sklearn.LGBMRegressor(boosting_type='gbdt', class_weight=None, 
-                                                                           colsample_bytree=1.0, importance_type='split', 
-                                                                           learning_rate=0.1, max_depth=-1, min_child_samples=20, 
-                                                                           min_child_weight=0.001, min_split_gain=0.0, 
-                                                                           n_estimators=100, n_jobs=-1, num_leaves=31, 
-                                                                           objective=None, random_state=42, reg_alpha=0.0, 
-                                                                           reg_lambda=0.0, silent='warn', subsample=1.0, 
-                                                                           subsample_for_bin=200000, subsample_freq=0))
-]
+forecasters = {
+    'naive_forecaster': {
+        'estimator': 
+            NaiveForecaster(sp=1, strategy='last', window_length=None)
+        ,  
+    },    
+    'naive_forecaster_seasonal': {
+        'estimator': 
+            NaiveForecaster(sp=12, strategy='last', window_length=None)
+        ,  
+    },
+    'polynomial_trend_forecaster': {
+        'estimator': 
+            PolynomialTrendForecaster(degree=1, regressor=None, with_intercept=True)
+        ,  
+    },    
+    'arima': {
+        'estimator': 
+            AutoARIMA(n_jobs=-1)
+        ,  
+    },    
+    'exponential_smoothing': {
+        'estimator': 
+            ExponentialSmoothing(damped_trend=False, initial_level=None, initial_seasonal=None, initial_trend=None, initialization_method='estimated', seasonal=None, sp=seasonal_period, trend='add', use_boxcox=None)
+        ,  
+    },    
+    'ets': {
+        'estimator': 
+            AutoETS(n_jobs=-1)
+        ,  
+    },      
+    'theta_forecaster': {
+        'estimator': 
+            ThetaForecaster(deseasonalize=True, initial_level=None, sp=seasonal_period)
+        ,  
+    },     
+    'decision_tree': {
+        'estimator': 
+            DirectTabularRegressionForecaster(estimator=DecisionTreeRegressor(ccp_alpha=0.0,  criterion='mse', max_depth=None, max_features=None, max_leaf_nodes=None, 
+                                  min_impurity_decrease=0.0, min_impurity_split=None, min_samples_leaf=1, min_samples_split=2, 
+                                  min_weight_fraction_leaf=0.0, random_state=42, splitter='best'))
+        ,  
+    },
+    'random_forest': {
+        'estimator': 
+            DirectTabularRegressionForecaster(estimator=RandomForestRegressor(bootstrap=True, ccp_alpha=0.0, criterion='mse', max_depth=None, max_features='auto', 
+                                  max_leaf_nodes=None, max_samples=None, min_impurity_decrease=0.0, min_impurity_split=None, 
+                                  min_samples_leaf=1, min_samples_split=2, min_weight_fraction_leaf=0.0, n_estimators=100, 
+                                  n_jobs=-1, oob_score=False, random_state=42, verbose=0, warm_start=False))
+        ,   
+    },    
+    'extra_trees': {
+        'estimator': 
+            DirectTabularRegressionForecaster(estimator=ExtraTreesRegressor(bootstrap=False, ccp_alpha=0.0, criterion='mse', max_depth=None, max_features='auto', 
+                                max_leaf_nodes=None, max_samples=None, min_impurity_decrease=0.0, min_impurity_split=None, 
+                                min_samples_leaf=1, min_samples_split=2, min_weight_fraction_leaf=0.0, n_estimators=100, 
+                                n_jobs=-1, oob_score=False, random_state=42, verbose=0, warm_start=False))
+        ,    
+    },     
+    'gradient_boosting': {
+        'estimator': 
+            DirectTabularRegressionForecaster(estimator=GradientBoostingRegressor(alpha=0.9, ccp_alpha=0.0, criterion='friedman_mse', init=None, learning_rate=0.1, loss='ls', 
+                                      max_depth=3, max_features=None, max_leaf_nodes=None, min_impurity_decrease=0.0, min_impurity_split=None, 
+                                      min_samples_leaf=1, min_samples_split=2, min_weight_fraction_leaf=0.0, n_estimators=100, n_iter_no_change=None, 
+                                      random_state=42, subsample=1.0, tol=0.0001, validation_fraction=0.1, verbose=0, warm_start=False))
+        ,   
+    },       
+    'adaboost': {
+        'estimator': 
+            DirectTabularRegressionForecaster(estimator=AdaBoostRegressor(base_estimator=None, learning_rate=1.0, loss='linear', n_estimators=50, random_state=42))
+        ,  
+    },      
+    'lgb_regressor': {
+        'estimator': 
+            DirectTabularRegressionForecaster(estimator=lgbm.sklearn.LGBMRegressor(boosting_type='gbdt', class_weight=None, colsample_bytree=1.0, importance_type='split', learning_rate=0.1, max_depth=-1, 
+                                       min_child_samples=20, min_child_weight=0.001, min_split_gain=0.0, n_estimators=100, n_jobs=-1, num_leaves=31, objective=None, 
+                                       random_state=42, reg_alpha=0.0, reg_lambda=0.0, silent='warn', subsample=1.0, subsample_for_bin=200000, subsample_freq=0))
+        ,    
+    },   
+    'knn': {
+        'estimator': 
+            DirectTabularRegressionForecaster(estimator=KNeighborsRegressor(algorithm='auto', leaf_size=30, metric='minkowski', metric_params=None, n_jobs=-1, n_neighbors=5, p=2, weights='uniform'))
+        ,    
+    },    
+    'passive_aggressive': {
+        'estimator': 
+            DirectTabularRegressionForecaster(estimator=PassiveAggressiveRegressor(C=1.0, average=False, early_stopping=False, epsilon=0.1, fit_intercept=True, loss='epsilon_insensitive', max_iter=1000, 
+                                       n_iter_no_change=5, random_state=42, shuffle=True, tol=0.001, validation_fraction=0.1, verbose=0, warm_start=False))
+        ,    
+    },       
+    'huber': {
+        'estimator': 
+            DirectTabularRegressionForecaster(estimator=HuberRegressor(alpha=0.0001, epsilon=1.35, fit_intercept=True, max_iter=100, tol=1e-05, warm_start=False))
+        ,    
+    },     
+    'bayesian_ridge': {
+        'estimator': 
+            DirectTabularRegressionForecaster(estimator=BayesianRidge(alpha_1=1e-06, alpha_2=1e-06, alpha_init=None, compute_score=False, copy_X=True, fit_intercept=True, lambda_1=1e-06, 
+                          lambda_2=1e-06, lambda_init=None, n_iter=300, normalize=False, tol=0.001, verbose=False))
+        ,    
+    },        
+    'lasso_lars': {
+        'estimator': 
+            DirectTabularRegressionForecaster(estimator=LassoLars(alpha=1.0, copy_X=True, eps=2.220446049250313e-16, fit_intercept=True, fit_path=True, jitter=None, max_iter=500, 
+                      normalize=True, positive=False, precompute='auto', random_state=42, verbose=False))
+        ,   
+    },        
+    'lars': {
+        'estimator': 
+            DirectTabularRegressionForecaster(estimator=Lars(copy_X=True, eps=2.220446049250313e-16, fit_intercept=True, fit_path=True, jitter=None, n_nonzero_coefs=500, 
+                 normalize=True, precompute='auto', random_state=42, verbose=False))
+        ,     
+    },       
+    'elastic_net': {
+        'estimator': 
+            DirectTabularRegressionForecaster(estimator=ElasticNet(alpha=1.0, copy_X=True, fit_intercept=True, l1_ratio=0.5, max_iter=1000, normalize=False, positive=False, 
+                       precompute=False, random_state=42, selection='cyclic', tol=0.0001, warm_start=False))
+        ,    
+    },        
+    'ridge': {
+        'estimator': 
+            DirectTabularRegressionForecaster(estimator=Ridge(alpha=1.0, copy_X=True, fit_intercept=True, max_iter=None, normalize=False, random_state=42, solver='auto', tol=0.001))
+        ,    
+    },     
+    'lasso': {
+        'estimator': 
+            DirectTabularRegressionForecaster(estimator=Lasso(alpha=1.0, copy_X=True, fit_intercept=True, max_iter=1000, normalize=False, positive=False, precompute=False, 
+                  random_state=42, selection='cyclic', tol=0.0001, warm_start=False))
+        ,    
+    },        
+}
+ML_forecasters = ['decision_tree', 'random_forest', 'extra_trees', 'gradient_boosting', 'adaboost', 'lgb_regressor', 
+                  'knn', 'passive_aggressive', 'huber', 'bayesian_ridge' , 'lasso_lars', 'lars', 'elastic_net', 
+                  'ridge', 'lasso']
 
 # ONLY FOR SKTIME
 # keep datetime as a column for plots
@@ -203,7 +250,13 @@ if sample == 'valid':
             # keep track of scores, per method and fh
             scores_expanding = pd.DataFrame()
 
-            for forecaster in forecasters:
+            for forecaster_name, value in forecasters.items():
+                forecaster = value['estimator']
+                # if ML forecaster
+                if forecaster_name in ML_forecasters:
+                    window_tuned_model = pd.read_pickle(f'../results/tuned_models/just_window/{dataset_name}/{target}.{forecaster_name}.pkl')
+                    window_length = window_tuned_model.get_params()['forecaster'].get_params()['window_length']
+                    forecaster = forecaster.set_params(window_length=window_length)
 
                 print('='*40, forecaster, '='*40)
                 min_max_scaler = TabularToSeriesAdaptor(MinMaxScaler(feature_range=(1, 2)))
@@ -238,7 +291,7 @@ if sample == 'valid':
                 p = predictions_valid.dropna()
 
                 scores_expanding = scores_expanding.append({
-                    'Method': str(forecaster), 
+                    'Method': str(forecaster).replace('\n', '').replace(' ', ''), 
                     'Forecasting Horizon': fh, 
                     'Preprocess': preprocess,
                     'Runtime': total_runtime,      
